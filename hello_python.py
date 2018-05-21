012f95a11a5c0757603a177c0542886684dadb9e
Murugan Nagarajan <murugan425@gmail.com>
2018-05-21 19:13:06 +0800

    Numbers, Variables, Strings in Python. Using both jupyter notebook & spyder IDE's for learning purpose.

---
+++ hello_python.py
@@ -0,0 +1,50 @@
+"""
+Created on Mon May 21 17:34:27 2018
+
+@author: Murugan_Nagarajan
+"""
+
+print ('Murugan Hello World Text')
+
+#Numbers
+0.1 + 0.2 - 0.3 #is not equal to 0.0 because of floating point arithmetic issue in machines
+
+3 - 3
+
+6**6
+
+6*6*6*6*6*6
+
+6**3 #equals 6*6*6
+
+1/2
+
+#Variables
+a = 10
+
+a = a + a
+a
+
+type(a)
+
+b = 40.4
+type(b)
+
+my_income = 15000
+tax_rate = 20
+my_tax_paid = my_income * tax_rate/100
+my_tax_paid
+
+#Strings
+name = "Murugan"
+sentence = "I'm trying to learn Machine Learning"
+
+print(name[0]+ "\n\t" + name[3])
+print(name[3])
+print(name[-1])
+print(name[-3])
+print(name[len(name)-1])
+
+len(name)
+len(sentence)
+
